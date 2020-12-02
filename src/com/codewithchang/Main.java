51d2b7d694fee1fb77772a2aa7bc8c622a16199b
Chang <cnxiong03@gmail.com>
2020-12-01 19:52:28 -0500

    switch and hashmap

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -1,13 +1,44 @@
 package com.codewithchang;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        Calculator calculator1 = new Calculator() {
-            @Override
-            public int Calculator(String input) {
-                return super.Calculator(input);
-            }
-        };
+        System.out.println(dnaPair("ACTG"));
+    }
+
+    /* {A: T, T: A, C: G, G: C} */
+    public static List<List<String>> dnaPair(String chain) {
+        HashMap<String, String> dnaMapping = new HashMap<>() {{
+            put("A", "T");
+            put("T", "A");
+            put("C", "G");
+            put("G", "C");
+        }};
+
+        List<List<String>> dnaChain = new ArrayList<>();
+        String[] chainArray = chain.split("");
+        for (var letter : chainArray) {
+            dnaChain.add(Arrays.asList(letter, dnaMapping.get(letter) ));
+//            switch (letter) {
+//                case "A":
+//                    dnaChain.add(Arrays.asList("A", "T"));
+//                    break;
+//                case "T":
+//                    dnaChain.add(Arrays.asList("T", "A"));
+//                    break;
+//                case "c":
+//                    dnaChain.add(Arrays.asList("C", "G"));
+//                    break;
+//                case "G":
+//                    dnaChain.add(Arrays.asList("G", "C"));
+//                    break;
+//            }
+        }
+        return dnaChain;
     }
 }
